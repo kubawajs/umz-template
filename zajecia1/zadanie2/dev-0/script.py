d3e1bf0bde3b93c3007bc68c23e8e2551fef1e8d
Jakub Wajs <kubawajs@interia.pl>
2018-04-06 23:31:49 +0200

    zadanie2 ready

--- zajecia1/zadanie2/dev-0/script.py
+++ zajecia1/zadanie2/dev-0/script.py
@@ -5,24 +5,21 @@ from sklearn import linear_model
 
 column_names = ['price', 'isNew', 'rooms', 'floor', 'location', 'sqrMetres']
 
-report = pd.read_csv('train.tsv', sep='\t', names = column_names)
+report = pd.read_csv('../train/train.tsv', sep='\t', names = column_names)
 
 report.describe()
 
-# report.plot()
-# plt.show()
-
-# sns.boxplot(y='price', data=report)
-# plt.show()
-
-# sns.violinplot( y='price', data=report)
-# plt.show()
-
 sns.regplot(y=report['price'], x=report['sqrMetres'])
-plt.show()
+img = plt.gcf()
+img.savefig('plot.png')
 
 reg = linear_model.LinearRegression()
 
 reg.fit(pd.DataFrame(report, columns=['sqrMetres']), report['price'])
 
-print(reg.predict(50)) # predict for 50 metres
+in_file_col_names = column_names[1:]
+in_file = pd.read_csv('in.tsv', sep='\t', names = in_file_col_names)
+
+result = reg.predict(pd.DataFrame(in_file['sqrMetres']))
+result = pd.Series(result)
+result.to_csv('out.tsv', sep='\t', header=False, index=False)
